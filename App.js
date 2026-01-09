5773813c3d9eca7b4d94a4e621a6a49b0e6db041
TheTilingWindow <aussiekareem@gmail.com>
2026-01-09 16:16:49 +1100

    Correct money count check in buyCookie function
    Fix conditional check for money count in buyCookie function.

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ function App() {
 
         setCookieCount((prev) => prev + 1);
         setMoneyCount((prev) => prev - 5);
-        if (moneyCount == 0) {
+        if (moneyCount = 0) {
             alert("You don't have enough money!");
         }
     }
